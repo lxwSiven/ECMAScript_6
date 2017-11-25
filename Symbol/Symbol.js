40e3488083efa21c6f51d4e68039561c8f5443de
lxwSiven <lxwsiven@lxwSivendeiMac.local>
2017-11-25 12:54:55 +0800

    symbol

--- Symbol/Symbol.js
+++ Symbol/Symbol.js
@@ -2,3 +2,24 @@
 
 // let s = Symbol();
 // console.log(typeof s); // symbol;
+
+// let s1 = Symbol('lxw');
+// let s2 = Symbol('lxx');
+// console.log(s1);  // Symbol(lxw)
+// console.log(s2);  // Symbol(lxx)
+// console.log(s1.toString());  // Symbol(lxw)
+// console.log(s2.toString());  // Symbol(lxx)
+
+// Symbol()参数是对象，自动调用对象toString方法。
+// const obj = {
+//     toString(){
+//         return 'abc';
+//     }
+//
+// };
+// const sym = Symbol(obj);
+// console.log(sym); // Symbol(abc)
+
+// let s1 = Symbol('lxw');
+// let s2 = Symbol('lxw');
+// console.log(s1 === s2); // false
