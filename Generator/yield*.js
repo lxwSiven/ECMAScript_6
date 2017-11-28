54a4b23982d96446023c03f95ac4ecfa3bd1936b
lxwSiven <lxwsiven@lxwSivendeiMac.local>
2017-11-28 17:53:05 +0800

    generator

--- Generator/yield*.js
+++ Generator/yield*.js
@@ -1,21 +1,39 @@
 'use strict'
 
-function* foo() {
+// function* foo() {
+//     yield 'a';
+//     yield 'b';
+// }
+//
+// function* bar() {
+//     yield 'x';
+//     // for(let v of foo()){
+//     //     console.log(v);
+//     // }
+//     // 两种写法效果一致
+//     yield* foo();
+//     // console.log(x);
+//     yield 'y';
+// }
+//
+// for(let v of bar()){
+//     console.log(v);
+// }
+
+
+function* genFuncWithReturn(){
     yield 'a';
     yield 'b';
+    return 'hello world!';
+}
+function* logReturned(genObj) {
+    let result = yield* genObj;
+    console.log(result);
 }
 
-function* bar() {
-    yield 'x';
-    // for(let v of foo()){
-    //     console.log(v);
-    // }
-    // 两种写法效果一致
-    yield* foo();
+// logReturned(genFuncWithReturn())
+// 返回一个遍历器对象
 
-    yield 'y';
-}
-
-for(let v of bar()){
-    console.log(v);
+for (let x of logReturned(genFuncWithReturn())){
+    console.log(x);
 }
